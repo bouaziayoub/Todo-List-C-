8fff172646da77f00c13e7d29812c4f7e7ac1a0e
bouaziayoub <bouaziayoub@gmail.com>
2024-10-28 14:49:38 +0100

    Fix loop and input handling in TODO application

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,4 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string inputUser = Console.ReadLine();
+
+while (inputUser.ToUpper() != "E")
+{
+    if (inputUser.ToUpper() == "A")
+    {
+        Console.WriteLine("You pressed A");
+    }
+    else if (inputUser.ToUpper() == "S")
+    {
+        Console.WriteLine("You pressed S");
+    }
+    else if (inputUser.ToUpper() == "R")
+    {
+        Console.WriteLine("You pressed R");
+    }
+    else
+    {
+        Console.WriteLine("You pressed something that is not in the menu!");
+    }
+
+    // Prompt for user input again at the end of each iteration
+    inputUser = Console.ReadLine();
+}
+
+Console.WriteLine("Exiting the program...");
+Console.ReadKey(); // Waits for the user to press a key
